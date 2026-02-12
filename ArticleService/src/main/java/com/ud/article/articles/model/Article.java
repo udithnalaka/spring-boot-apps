d9af47d2cf613cd3621b4b52792cc9efe47c9bc0
udithnalaka <udithnalaka@yahoo.com.au>
2026-02-12 11:13:55 +1000

    dockerizing the application for both springboot app and postgres

--- ArticleService/src/main/java/com/ud/article/articles/model/Article.java
+++ ArticleService/src/main/java/com/ud/article/articles/model/Article.java
@@ -25,7 +25,7 @@ import java.util.List;
 public class Article {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String content;
--- ArticleService/src/main/java/com/ud/article/articles/model/Tag.java
+++ ArticleService/src/main/java/com/ud/article/articles/model/Tag.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 public class Tag {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "article_id")
