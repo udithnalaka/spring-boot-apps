227c0afb5c6b10ccb155ec4f0898d1ae5f184aa1
udithnalaka <udithnalaka@yahoo.com.au>
2026-02-16 17:17:46 +1000

    add spring security with Auth0 support for resource server

--- ArticleService/src/main/java/com/ud/article/articles/ArticleServiceApplication.java
+++ ArticleService/src/main/java/com/ud/article/articles/ArticleServiceApplication.java
@@ -3,8 +3,10 @@ package com.ud.article.articles;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 @EnableCaching
 public class ArticleServiceApplication {
 
